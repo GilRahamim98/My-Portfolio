f125ad012040f7ccba3cf55c45668a08d0b8b29e
Gil <frogvsdogvsmarsmelo@gmail.com>
2023-02-01 19:06:21 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Portfolio from './components/portfolio/Portfolio';
 import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 function App() {
   return (
     <div >
@@ -14,6 +15,7 @@ function App() {
       <Skills />
       <Portfolio />
       <Contact />
+      <Footer />
 
     </div>
   );
