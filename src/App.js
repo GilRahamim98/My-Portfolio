27517d7cc35d5c816a78d03b914d92fb67aba2ae
Gil <frogvsdogvsmarsmelo@gmail.com>
2023-02-01 13:35:04 +0200

    Added about

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Home from './components/home/Home';
 import Navbar from './components/navbar/Navbar'
-
+import About from './components/about/About';
 function App() {
   return (
     <div >
       <Navbar />
       <Home />
+      <About />
 
     </div>
   );
---
+++ src/components/about/About.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import './About.scss'
+import { motion } from 'framer-motion';
+import portfolio from '../../assets/portfolio.jpeg'
+import { bios } from '../../Data';
+import cv from '../../assets/CV Gil Rahamim.pdf'
+
+const About = () => {
+    return (
+        <div className='container' id='about'>
+            <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ y: [-50, 0], opacity: 1 }}
+                className="title">
+                <span>Who Am I?</span>
+                <h1>About Me</h1>
+            </motion.div>
+            <div className="about_container">
+                <motion.div
+                    initial={{ x: 0, opacity: 0 }}
+                    whileInView={{ x: [-250, 0], opacity: 1 }}
+                    transition={{ duration: 1 }}
+                    className="about_left">
+                    <motion.img src={portfolio}
+                        whileHover={{ y: -48, x: -55 }}
+                        transition={{ duration: 0.3 }}
+                    />
+                </motion.div>
+                <motion.div className="about_right"
+                    initial={{ x: 0, opacity: 0 }}
+                    whileInView={{ x: [250, 0], opacity: 1 }}
+                    transition={{ duration: 1 }}
+                >
+
+                    <p>A creative and motivated individual with a passion for web development, seeking opportunities to expand knowledge and contribute to challenging projects.</p>
+                    {bios.map(bio => {
+                        return (
+                            <div className="bio" key={bio.id}>
+                                <span className='bioKey'>{bio.icon}{bio.key}</span>
+                                <span className='bioValue'>{bio.value}</span>
+                            </div>
+                        )
+                    })}
+                    <motion.a href={cv} download
+                        whileHover={{ scale: 1.1 }}
+                        transition={{ duration: 0.3 }}
+                    >
+                        Download CV
+                    </motion.a>
+                </motion.div>
+
+            </div>
+        </div>
+    )
+}
+
+export default About
